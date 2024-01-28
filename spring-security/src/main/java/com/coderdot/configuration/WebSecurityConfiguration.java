c4c315ded3c280fc18997a8760917eaac0e270a4
Achraftraf <achrafzarouki20@gmail.com>
2024-01-28 22:12:28 +0100

    fix problem of security

--- spring-security/src/main/java/com/coderdot/configuration/WebSecurityConfiguration.java
+++ spring-security/src/main/java/com/coderdot/configuration/WebSecurityConfiguration.java
@@ -1,9 +1,7 @@
-package com.coderdot.controllers;
+package com.coderdot.configuration;
 
-import java.util.List;
-
+import com.coderdot.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -34,7 +32,7 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity security) throws Exception {
         return security.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/signup", "/login","/error","/formateurs/**").permitAll()
+                .requestMatchers("/signup", "/login","/error","/formateurs/**" , "/inscrire").permitAll()
                 .and()
                 .authorizeHttpRequests().requestMatchers("/api/**")
                 .authenticated()
@@ -58,67 +56,5 @@ public class WebSecurityConfiguration {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
-// =======
-// import org.springframework.http.HttpStatus;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.security.access.prepost.PreAuthorize;
-// import org.springframework.web.bind.annotation.CrossOrigin;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
 
-// import com.coderdot.entities.Formateur;
-// import com.coderdot.services.FormateurService;
-
-// @RestController
-// //@RequestMapping("/api/formateurs")
-// @CrossOrigin(origins = "*")
-// public class FormateurController {
-// >>>>>>> lamiae_2
-
-	@Autowired
-	private FormateurService formateurService;
-	
-	@PostMapping("/inscrire")
-	public ResponseEntity<Formateur> inscrireFormateur(@RequestBody Formateur formateur) {
-     Formateur inscritFormateur = formateurService.inscrireFormateur(formateur);
-     return new ResponseEntity<>(inscritFormateur, HttpStatus.CREATED);
-	}
-	 @GetMapping("/api/formateurs/all")
-	 @PreAuthorize("hasRole('ROLE_ADMIN')")
-	    public ResponseEntity<List<Formateur>> getAllFormateurs() {
-	        List<Formateur> allFormateurs = formateurService.getAllFormateurs();
-	        return new ResponseEntity<>(allFormateurs, HttpStatus.OK);
-	    }
-	
-
-	 	@PutMapping("/api/formateurs/accepter/{formateurId}")
-	    @PreAuthorize("hasRole('ROLE_ADMIN')")
-	    public ResponseEntity<Formateur> accepterFormateur(@PathVariable Long formateurId) {
-	        try {
-	            Formateur formateurAccepte = formateurService.acceptFormateur(formateurId);
-	            return new ResponseEntity<>(formateurAccepte, HttpStatus.OK);
-	        } catch (Exception e) {
-	            e.printStackTrace(); 
-	            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-	        }
-	    }
-
-	 	@DeleteMapping("/api/formateurs/refuser/{formateurId}")
-	    @PreAuthorize("hasRole('ROLE_ADMIN')")
-	    public ResponseEntity<Void> refuserFormateur(@PathVariable Long formateurId) {
-	        try {
-	            formateurService.refuseFormateur(formateurId);
-	            return new ResponseEntity<>(HttpStatus.OK);
-	        } catch (Exception e) {
-	            e.printStackTrace(); 
-	            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-	        }
-	    }
 }
-
